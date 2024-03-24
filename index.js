c2b8b075e150d297ed449277201c4e6eb6c23442
joy <blackbearmode1@gmail.com>
2024-03-24 17:17:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendcat(name) {
+    return {...cats, name};
+}
+
+function prependCat(name) {
+    return {name, ...cats};
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+module.export = { cats, destructivelyPrependCat, destructivelyRemoveLastCat, destructivelyRemoveFirstCat, appendcat, prependCat, removeLastCat, removeFirstCat };
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+module.exports ={ cats, destructivelyAppendCat};
